f1b2624c8e0d56d8e09a0e854714a8364691e4d5
SahilChaudhari <sahilchaudhari@SahilAir.local>
2024-03-25 21:17:03 +0530

    fix some code again

--- script.groovy
+++ script.groovy
@@ -1,19 +1,3 @@
-// /* groovylint-disable-next-line FactoryMethodName */
-// def buildJar() {
-//     echo(message: 'build application ')
-//     sh 'mvn package'
-// }
-// def buildImage(IMAGE_VERSION) {
-//     echo "Building Docker image with version ${IMAGE_VERSION}"
-//     sh "docker build -t sahilchaudhari2405/my-repo:${IMAGE_VERSION} ."
-// }
-// def loginAndpush(IMAGE_VERSION) {
-//     echo 'Logging in to Docker'
-    
-// }And here's the script.groovy file:
-
- 
-
 def buildApp() {
     echo "building the application..."
     sh 'mvn package'
