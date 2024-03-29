2e4b5c7713894bbf0789f0f7d2bc6e7cedd2c85f
SahilChaudhari <sahilchaudhari@SahilAir.local>
2024-03-29 16:38:42 +0530

    some change

--- script.groovy
+++ script.groovy
@@ -4,13 +4,13 @@ def buildApp() {
 } 
 def buildImage(IMAGE_VERSION) {
     echo "build image"
-    sh "docker build -t sahilchaudhari2405/my-repo:${IMAGE_VERSION} ."
+    sh "docker build -t sahilchaudhari2405/java-mevan-app:${IMAGE_VERSION} ."
     }
 def deployApp(IMAGE_VERSION) {
     echo "deploying the ${IMAGE_VERSION}"
     withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
         sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
-        sh "docker push sahilchaudhari2405/my-repo:${IMAGE_VERSION}"
+        sh "docker push 159.89.174.141:8083/java-mevan-app:${IMAGE_VERSION}"
     }
 } 
 return this
