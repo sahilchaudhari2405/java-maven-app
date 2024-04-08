b9b222f0634d1b7cd41cc458179c534f00896a46
sahil chaudhari <82030235+sahilchaudhari2405@users.noreply.github.com>
2024-04-08 18:52:01 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,15 +2,15 @@ def buildApp() {
     echo "building the application..."
     sh 'mvn package'
 } 
-def buildImage(IMAGE_VERSION,IMAGE_NAME) {
-    echo "build images"
-    sh "docker build -t 159.89.174.141:8083/${IMAGE_NAME}:${IMAGE_VERSION} ."
+def buildImage(IMAGE_VERSION) {
+    echo "build image"
+    sh "docker build -t sahilchaudhari2405/my-repo:${IMAGE_VERSION} ."
     }
-def deployApp(IMAGE_VERSION,IMAGE_NAME) {
+def deployApp(IMAGE_VERSION) {
     echo "deploying the ${IMAGE_VERSION}"
-    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-        sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} 159.89.174.141:8083"
-        sh "docker push 159.89.174.141:8083/${IMAGE_NAME}:${IMAGE_VERSION}"
+    withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+        sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
+        sh "docker push sahilchaudhari2405/my-repo:${IMAGE_VERSION}"
     }
 } 
 return this
